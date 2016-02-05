09c27eed0c228ce02de27626f37944f12a7b0a38
Andre Augusto <andre.cavalcanti@corp.globo.com>
2016-02-05 10:34:23 -0200

    test suite: fixing the way the tests are starting

--- setup.py
+++ setup.py
@@ -1,13 +1,24 @@
-#from distutils.core import setup
 from setuptools import setup
 
 version = '1.5.0'
 
-setup(name='staticgenerator',
-      version=version,
-      description="StaticGenerator for Django",
-      author="Jared Kuolt",
-      author_email="me@superjared.com",
-      url="http://superjared.com/projects/static-generator/",
-      packages = ['staticgenerator']
-      )
+tests_require = [
+    'coverage',
+    'mox',
+    'django-nose',
+    'nose',
+    'django==1.4.22',
+]
+
+setup(
+    name='staticgenerator',
+    version=version,
+    description="StaticGenerator for Django",
+    author="Jared Kuolt",
+    author_email="me@superjared.com",
+    url="http://superjared.com/projects/static-generator/",
+    packages=['staticgenerator'],
+    extras_require={
+        'tests': tests_require,
+    },
+)
