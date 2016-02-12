bdb2c9ade2b797184e2b980c22024555b746ea3b
Andre Augusto <andre.cavalcanti@corp.globo.com>
2016-02-12 11:40:18 -0200

    handlers: fixing a bug that broke the static generation if the middleware_method was only a function. This happens for instance with NewRelics middleware

--- staticgenerator/handlers.py
+++ staticgenerator/handlers.py
@@ -11,7 +11,10 @@ class DummyHandler(BaseHandler):
     def __call__(self, request):
         self.load_middleware()
         response = self.get_response(request)
+
         for middleware_method in self._response_middleware:
-            if not isinstance(middleware_method.im_self, TransactionMiddleware):
-                response = middleware_method(request, response)
+            if hasattr(middleware_method, 'im_self') and isinstance(middleware_method.im_self, TransactionMiddleware):
+                continue
+
+            response = middleware_method(request, response)
         return response
